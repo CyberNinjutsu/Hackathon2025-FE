56448c877c5f079924a28a71071d41fe0f9ab393
BuiChiBao <buichibao1601@gmail.com>
2025-09-08 16:03:26 +0700

    feat (layout): custom scrollbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import type { Metadata } from "next"
 import { GeistSans } from "geist/font/sans"
+import { GeistMono } from "geist/font/mono"
 import { Analytics } from "@vercel/analytics/next"
 import { Suspense } from "react"
 import "./globals.css"
@@ -18,7 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en' className='dark antialiased'>
-      <body className={`font-sans ${GeistSans.variable} ${GeistMono.variable}`}>
+      <body
+        className={`font-sans ${GeistSans.variable} ${GeistMono.variable} custom-scrollbar`}
+      >
         <Suspense fallback={null}>{children}</Suspense>
         <Analytics />
       </body>
