89d59f39d587007082015f709dbed6b1347d369c
BuiChiBao <buichibao1601@gmail.com>
2025-09-19 22:51:06 +0700

    fix: add type

--- src/utils/Types.ts
+++ src/utils/Types.ts
@@ -1,28 +1,28 @@
 interface Asset {
-  id: string;
-  name: string;
-  symbol: string;
-  icon: React.ReactNode;
-  amount: number;
-  value: number;
-  percentage: number;
-  color: string;
+  id: string
+  name: string
+  symbol: string
+  icon: React.ReactNode
+  amount: number
+  value: number
+  percentage: number
+  color: string
 }
 
 interface Transaction {
-  id: string;
-  type: "buy" | "sell" | "receive" | "send";
-  description: string;
-  timestamp: string;
-  amount: number;
-  status: "success" | "pending" | "failed";
+  id: string
+  type: "buy" | "sell" | "receive" | "send" | "other"
+  description: string
+  timestamp: string
+  amount: number
+  status: "success" | "pending" | "failed"
 }
 
 interface AssetHistory {
-  id: string;
-  name: string;
-  color: string;
-  data: { date: string; value: number }[];
+  id: string
+  name: string
+  color: string
+  data: { date: string; value: number }[]
 }
 
-export type { Asset, Transaction, AssetHistory };
+export type { Asset, Transaction, AssetHistory }
